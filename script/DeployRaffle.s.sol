4ed8c0ae0ff90ce977cca4f3a25dbcf2259feeb3
Rohit <rohit.bhalerao591@gmail.com>
2023-07-17 23:56:39 +0530

    Update code

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,9 +3,30 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployRaffle is Script{
+contract DeployRaffle is Script {
     function run() external returns (Raffle) {
-      
+        HelperConfig helperConfig = HelperConfig();
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
     }
 }
+
+//  (
+//             uint256 entranceFee,
+//             uint256 interval,
+//             address vrfCoordinator,
+//             bytes32 gasLane,
+//             uint64 subscriptionId,
+//             uint32 callbackGasLimit
+//         ) = helperConfig.activeNetworkConfig();
+//         This can be similiar to when we import NetworkConfig from HelperConfig.s.sol
+//         and then // NetworkConfig memory networkConfig = helperConfig.activeNetworkConfig();
+//         But we are destructuring the networkconfig object into its underlying parameters
